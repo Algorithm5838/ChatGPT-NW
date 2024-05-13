4bd30b46ca3289efa06d03a96baaf1198f0fda1e
Algorithm5838 <108630393+Algorithm5838@users.noreply.github.com>
2024-05-13 22:38:38 +0300

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -292,6 +292,7 @@ export function isVisionModel(model: string) {
     "claude-3",
     "gemini-1.5-pro",
     "gemini-pro-1.5",
+    "gpt-4o",
   ];
 
   const isGpt4Turbo = model.includes("gpt-4-turbo") && !model.includes("preview");
